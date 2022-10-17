4fbb36061b020f6db90298b352a1016d4e160af1
darshu962 <chaudharidarshan223@gmail.com>
2022-10-17 10:59:27 +0530

    project configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os 
+CSV_FILE_PATH = 'ENTER_PATH'
+
+PORT_NUMBER = 5505
+
+MODEL_FILE_PATH = os.path.join("models", "linear_model.pkl")
+
+JSON_FILE_PATH = os.path.join("models", "json_data.json")
+
+
